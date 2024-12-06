ce4fd01954a65a19869845d9e15a11f3a48c242b
Divinegirl8 <iamoluchimercy@gmail.com>
2024-12-06 08:54:46 +0100

    new commit

--- src/component/Portfolio/index.tsx
+++ src/component/Portfolio/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import passGen from "../../../src/asset/portfolio/passgen.png"
+import dashboard from "../../asset/portfolio/dashboard-user.png"
+import blog from "../../asset/portfolio/blog.png"
 
 const Portfolio:React.FC = () =>{
     return(
-        <div className='bg-[#000] w-full h-full pb-96'>
+        <div className='bg-[#000] w-full h-full '>
 
         <div className="flex ml-[30rem] pt-[10rem]">
               <div className="flex flex-row gap-2 justify-center border-gray-500 border w-[10rem] h-[30px] rounded-[20px] pt-1">
@@ -38,6 +40,9 @@ const Portfolio:React.FC = () =>{
                                 Render
                               </span>
                               <span className="bg-black text-white px-4 py-2 rounded-md mx-2 text-sm font-medium">
+                                TailwindCss
+                              </span>
+                              <span className="bg-black text-white px-4 py-2 rounded-md mx-2 text-sm font-medium">
                                 React
                               </span>
                               <span className="bg-black text-white px-4 py-2 rounded-md mx-2 text-sm font-medium">
@@ -67,6 +72,70 @@ const Portfolio:React.FC = () =>{
 
                   
             </div>
+
+            <div className="ml-[30rem]  mt-10">
+            <div className="relative group ">
+                          {/* Image */}
+                          <img
+                            src={dashboard}
+                            className="w-[50rem] h-[30rem] object-cover transition-opacity duration-300 group-hover:opacity-75"
+                          />
+
+                          {/* Skills */}
+                          <div className="top-[27rem] ml-10 absolute inset-0">
+                               <span className="bg-black text-white px-4 py-2 rounded-md mx-2 text-sm font-medium">
+                                React
+                              </span>
+                              <span className="bg-black text-white px-4 py-2 rounded-md mx-2 text-sm font-medium">
+                                TypeScript
+                              </span>
+                              <span className="bg-black text-white px-4 py-2 rounded-md mx-2 text-sm font-medium">
+                                GraphqlZero
+                              </span>
+                              <span className="bg-black text-white px-4 py-2 rounded-md mx-2 text-sm font-medium">
+                                Vercel
+                              </span>
+                              <span className="bg-black text-white px-4 py-2 rounded-md mx-2 text-sm font-medium">
+                                Redux
+                              </span>
+                              <span className="bg-black text-white px-4 py-2 rounded-md mx-2 text-sm font-medium">
+                                TailwindCss
+                              </span>
+                            </div>
+
+
+                          <div className="absolute inset-0 ml-[-10rem] flex flex-col items-center justify-center  opacity-0 bg-black bg-opacity-30 group-hover:opacity-100 transition-opacity duration-300">
+                          
+                          <p className="text-white text-center mb-4 px-4 w-[40rem]">
+                             A simple project that retrieves user data from GraphQLZero, displays it in a table, and includes features like pagination, filtering, and searching. Additionally, it provides a modal to show detailed information about a selected user.
+                          </p>
+                            {/* Buttons */}
+                            <div>
+                              <button className="bg-white text-black px-4 py-2 rounded-md mx-2 hover:bg-gray-200">
+                                <a href="https://dashboard-zeta-one-11.vercel.app/" target="_blank">go to website</a>
+                              </button>
+
+                              <button className="bg-white text-black px-4 py-2 rounded-md mx-2 hover:bg-gray-200">
+                                <a href="https://github.com/Divinegirl8/Dashboard" target="_blank">go to github</a>
+                              </button>
+                            </div>
+
+                          </div>
+
+
+                          
+            </div>
+
+
+            
+          
+            </div>
+
+
+            
+
+
+            
 
             
 
