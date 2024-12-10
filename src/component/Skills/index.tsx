07bf4fc9a0082461d93b514fecc22b4131fcde71
Divinegirl8 <iamoluchimercy@gmail.com>
2024-12-10 08:33:45 +0100

    new commit

--- src/component/Skills/index.tsx
+++ src/component/Skills/index.tsx
@@ -35,131 +35,145 @@ import keycloak from "../../asset/skills/Keycloak_Logo-removebg-preview.png"
 import swagger from "../../asset/skills/swagger-removebg-preview.png"
 import mockito from "../../asset/skills/mockito-removebg-preview.png"
 import flutter from "../../asset/skills/flutter-removebg-preview.png"
+import style from "./index.module.css"
 
 
+const Skills:React.FC = () =>{
+    return(
+      
+        <div className='bg-[#343434] w-full h-full'>
+
+        <div className="flex ml-[30rem] pt-[10rem]">
+              <div className="flex flex-row gap-2 justify-center border-gray-500 border w-[10rem] h-[30px] rounded-[20px] pt-1">
+                <i
+                  className="fa-solid fa-briefcase"
+                  style={{ color: "#fff", paddingTop: "3px", fontSize: "13px" }}
+                ></i>
+                <p className="font-normal text-white font-fontInter uppercase text-[13px]">Skills</p>
+              </div>
+            </div>
 
 
+            <h1 className="text-white ml-[30rem] text-[50px] mt-10">Technologies I Use</h1>
 
-import { motion, useAnimation } from "framer-motion";
-import { useEffect, useRef } from "react";
+            <div className="ml-[30rem] mt-10 w-[50%] h-auto bg-white rounded-[10px] p-4">
+                <div className="grid grid-cols-2 lg:grid-cols-9 gap-5 sm:grid-cols-2 md:grid-cols-3">
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={java} className="w-20 h-20" title="java" />
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={react} className="w-16 h-16" title="react js" />
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={js} className="w-16 h-16" title="javascript"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={vue} className="w-16 " title="vue js"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={bootstrap} className="w-14 h-14" title="bootstrap"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={apollo} className="w-16 h-16" title="apollo"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={nodejs} className="w-16 h-16" title="node js"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={redux} className="w-14 h-14" title="redux"/>
+                  </div>
+                   <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={css} className="w-16 h-16" title="css"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={html} className="w-16 h-16" title="html"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={tsx} className="w-20 h-16" title="typescript"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={junit} className="w-16 h-16" title="junit"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={render} className="w-16 h-16" title="render"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={pytest} className="w-16 h-16" title="pytest"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={materialUi} className="w-16 h-16" title="material ui"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={docker} className="w-16 h-16" title="docker"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={git} className="w-16 h-16" title="git"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={figma} className="w-16 h-16" title="figma"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={jbpm} className="w-16 h-16" title="jbpm"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={python} className="w-20 h-20" title="python"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={next} className="w-14 h-14" title="next js"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={reactNative} className="w-16 h-16" title="react native" />
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={springBoot} className="w-20 h-20" title="spring boot"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={tailwindcss} className="w-20 h-20" title="tailwindcss" />
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={mongodb} className="w-16 h-16" title="mongodb"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={pg} className="w-16 h-16" title="postgres"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={mysql} className="w-16 h-14" title="mysql"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={postman} className="w-16 h-16" title="postman"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={linux} className="w-16 h-16" title="linux"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={macos} className="w-16 h-14" title="macOs"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={windows} className="w-16 h-14" title="windows"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={go} className="w-[85px] h-20" title="go"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={keycloak} className="w-16 h-16" title="keycloak" />
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={swagger} className="w-16 h-16" title="swagger" />
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={mockito} className="w-16 h-14" title="mockito"/>
+                  </div>
+                  <div className={`flex justify-center items-center ${style.iconContainer}`}>
+                    <img src={flutter} className="w-16 h-14" title="flutter" />
+                  </div>
 
-const Skills: React.FC = () => {
-  const controls = useAnimation();
-  const skillsRef = useRef<HTMLDivElement>(null);
+                </div>
+              </div>
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          controls.start("visible");
-        } else {
-          controls.start("hidden");
-        }
-      },
-      { threshold: 0.2 }
-    );
-
-    if (skillsRef.current) {
-      observer.observe(skillsRef.current);
-    }
-
-    return () => {
-      if (skillsRef.current) {
-        observer.unobserve(skillsRef.current);
-      }
-    };
-  }, [controls]);
-
-  const skillVariants = {
-    hidden: { opacity: 0, scale: 0.8 },
-    visible: { opacity: 1, scale: 1, transition: { duration: 0.5, staggerChildren: 0.1 } },
-  };
-
-  return (
-    <div ref={skillsRef} className="bg-[#343434] w-full h-full">
-      <div className="flex ml-[30rem] pt-[10rem]">
-        <div className="flex flex-row gap-2 justify-center border-gray-500 border w-[10rem] h-[30px] rounded-[20px] pt-1">
-          <i
-            className="fa-solid fa-briefcase"
-            style={{ color: "#fff", paddingTop: "3px", fontSize: "13px" }}
-          ></i>
-          <p className="font-normal text-white font-fontInter uppercase text-[13px]">
-            Skills
-          </p>
         </div>
-      </div>
+     
+    )
 
-      <h1 className="text-white ml-[30rem] text-[50px] mt-10">
-        Technologies I Use
-      </h1>
-
-      <motion.div
-        className="ml-[30rem] mt-10 w-[50%] h-auto bg-white rounded-[10px] p-4"
-        initial="hidden"
-        animate={controls}
-        variants={skillVariants}
-      >
-         <div className="grid grid-cols-2 lg:grid-cols-9 gap-5 sm:grid-cols-2 md:grid-cols-3">
-          {[
-            { src: java, title: "java" },
-            { src: react, title: "react js" },
-            { src: js, title: "javascript" },
-            { src: vue, title: "vue js" },
-            { src: bootstrap, title: "bootstrap" },
-            { src: apollo, title: "apollo" },
-            { src: nodejs, title: "node js" },
-            { src: redux, title: "redux" },
-            { src: css, title: "css" },
-            { src: html, title: "html" },
-            { src: tsx, title: "typescript" },
-            { src: junit, title: "junit" },
-            { src: render, title: "render" },
-            { src: pytest, title: "pytest" },
-            { src: materialUi, title: "material ui" },
-            { src: docker, title: "docker" },
-            { src: git, title: "git" },
-            { src: figma, title: "figma" },
-            { src: jbpm, title: "jbpm" },
-            { src: python, title: "python" },
-            { src: next, title: "next js" },
-            { src: reactNative, title: "react native" },
-            { src: springBoot, title: "spring boot" },
-            { src: tailwindcss, title: "tailwindcss" },
-            { src: mongodb, title: "mongodb" },
-            { src: pg, title: "postgres" },
-            { src: mysql, title: "mysql" },
-            { src: postman, title: "postman" },
-            { src: linux, title: "linux" },
-            { src: macos, title: "macOs" },
-            { src: windows, title: "windows" },
-            { src: go, title: "go" },
-            { src: keycloak, title: "keycloak" },
-            { src: swagger, title: "swagger" },
-            { src: mockito, title: "mockito" },
-            { src: flutter, title: "flutter" },
-          ].map((item, index) => (
-            <motion.div
-              key={index}
-              className="flex justify-center items-center"
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{
-                duration: 0.5,
-                delay: index * 0.1,
-                ease: "easeInOut",
-              }}
-              whileHover={{ scale: 1.2 }}
-            >
-              <img src={item.src} className="w-16 h-16" title={item.title} />
-            </motion.div>
-          ))}
-        </div>
-      </motion.div>
-    </div>
-  );
-};
+}
 
 export default Skills;
-
-
-
