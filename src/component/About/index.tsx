2bcd10f40ac1ac901bba7a8635a94f177e9e554f
Divinegirl8 <iamoluchimercy@gmail.com>
2024-12-16 16:51:29 +0100

    new commit

--- src/component/About/index.tsx
+++ src/component/About/index.tsx
@@ -184,7 +184,7 @@ const About = React.forwardRef<HTMLDivElement, {}>((props, ref)=> {
   });
 
   const slideIn3 = useSpring({
-    transform: inView3 ? "translateY(0)" : "translateY(100%)",
+    transform: inView3 ? "translateY(0)" : "translateY(70%)",
     opacity: inView3 ? 1 : 0,
     config: { tension: 100, friction: 50 },
   });
