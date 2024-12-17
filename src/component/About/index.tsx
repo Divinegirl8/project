4555b9879910fb2db86706602207464aa8cf0e24
Divinegirl8 <iamoluchimercy@gmail.com>
2024-12-17 13:01:35 +0100

    new commit

--- src/component/About/index.tsx
+++ src/component/About/index.tsx
@@ -193,7 +193,7 @@ const About = React.forwardRef<HTMLDivElement, {}>((props, ref)=> {
     <>
    
       <div className={style.header} ref={ref}>
-        <div className={`text-white lg:justify-center lg:pb-[5.5rem] lg:items-center lg:flex lg:ml-10 overflow-y-hidden ${style.abtDiv}`}>
+        <div className={`text-white lg:justify-center lg:pb-[5.5rem] lg:items-center lg:flex xl:ml-10 lg:ml-[8rem] overflow-y-hidden ${style.abtDiv}`}>
          
        
         <div className="justify-center items-center flex lg:pt-10">
@@ -232,7 +232,7 @@ const About = React.forwardRef<HTMLDivElement, {}>((props, ref)=> {
               <animated.div ref={ref3} style={slideIn3}>
               <p
                 ref={(el) => (elementsRef.current[2] = el)}
-                className={`${style.abtTxt} lg:max-w-xs  xl:max-w-xl  lg:mx-0 lg:ml-5 mx-6 text-[#999999]`}
+                className={`${style.abtTxt} lg:max-w-[35rem]  xl:max-w-xl  lg:mx-0 lg:ml-5 mx-6 text-[#999999]`}
               >
                
                I'm a dedicated Software Engineer with expertise in crafting scalable, 
--- src/component/HeroPage/index.tsx
+++ src/component/HeroPage/index.tsx
@@ -26,17 +26,17 @@ const HeroPage = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
         <div className={`cursor-none bg-gradient-to-b from-black via-black to-transparent lg:pb-[15rem] pb-[45rem]  relative  ${style.mainBox}`} ref={ref}>
             <div className="">
 
-                <div className={`border-gray-500 border-[0.5px] lg:w-[21rem] h-[39rem] mt-5  px-2 mx-5 lg:px-0 rounded-[20px] lg:left-5 lg:top-[10px] lg:my-[5rem] sm:mx-[2rem] lg:fixed z-50 ${style.boxIntro}`}>
+                <div className={`border-gray-500 border-[0.5px] xl:w-[21rem] lg:h-[32rem] xl:h-[39rem] h-[39rem] mt-5  px-2 mx-5 lg:px-0 rounded-[20px] lg:left-5 lg:top-[10px] lg:my-[5rem] sm:mx-[2rem] lg:fixed z-50 ${style.boxIntro}`}>
                    
                    <div className="mt-5 ml-7">
-                   <img src={logo} className={`w-[9rem] ${style.myLogo}`} />
+                   <img src={logo} className={`w-[9rem] xl:w-[9rem] lg:w-[6rem] ${style.myLogo}`} />
                    </div>
 
                    <div className="flex justify-center mt-7">
-                   <img src={pix2} className={`w-[14rem] h-[14rem] rounded-[20px] ${style.myFoto}`} />
+                   <img src={pix2} className={`xl:w-[14rem] xl:h-[14rem] lg:w-[12rem] lg:h-[12rem] w-[14rem] h-[14rem] rounded-[20px] ${style.myFoto}`} />
                    </div>
-                    <h1 className={`text-white text-[20px] text-center mt-10 ${style.myText}`}>divinemercy601@gmail.com</h1>
-                    <p className={`text-yellow-600 text-center text-[12px] ${style.copy}`}>© 2024 DivineMercy. All Rights Reserved</p>
+                    <h1 className={`text-white text-[20px] xl:text-[20px] lg:text-[15px] text-center mt-10 ${style.myText}`}>divinemercy601@gmail.com</h1>
+                    <p className={`text-yellow-600 text-center xl:text-[12px] lg:text-[9px] text-[12px] ${style.copy}`}>© 2024 DivineMercy. All Rights Reserved</p>
                     
                     <div className="flex flex-row gap-5  items-center justify-center mt-10">
 
@@ -65,7 +65,7 @@ const HeroPage = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
 
                    </div>
                     
-                    <div className={`lg:flex lg:items-center justify-center mt-16 ${style.butn}`}>
+                    <div className={`lg:flex lg:items-center justify-center xl:mt-16 lg:mt-[35px] mt-16 ${style.butn}`}>
                         <div onClick={handleModalToggle}>
                         <FilledButton text="Contact Me"/>
                         </div>
@@ -102,7 +102,7 @@ const HeroPage = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
 
 
                  <div className="lg:flex flex-col text-white justify-center items-center mt-[10rem] hidden">
-                <div className={`mr-[22rem] ${style.introduce}`}>
+                <div className={`xl:mr-[22rem] mr-[22rem] lg:mr-[14rem] ${style.introduce}`}>
                 <div className="flex flex-row gap-2 items-center justify-center border-gray-500 border w-[10rem] h-[30px] rounded-[20px]">
                     <i className="fa-solid fa-house" style={{ color: "#fff", fontSize: "13px" }}></i>
                     <p className="font-normal text-white font-fontInter uppercase lg:text-[13px] text-[12px]">Introduce</p>
@@ -117,7 +117,7 @@ const HeroPage = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
                     <p>A Software Engineer</p>
                     </div> */}
 
-                      <div className={`font-normal text-[68px] text-white font-fontInter ml-[7rem] mt-[6rem] ${style.name}`}>
+                      <div className={`font-normal xl:text-[68px] lg:text-[50px] text-[68px] text-white font-fontInter xl:ml-[7rem] ml-[7rem] lg:ml-[5rem] mt-[6rem] ${style.name}`}>
                     <p>
                         Hi, I am James <span className={`${style.first} ml-[20px] `}> DivineMercy</span><span className={` ${style.second}  ml-[20px]`}>DivineMercy,</span>
                     </p>
@@ -151,13 +151,13 @@ const HeroPage = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
 
                 <div className="text-white  hidden justify-center  mt-[20rem] lg:flex flex-row gap-16 mr-[10rem]">
                    <div>
-                   <h1 className={`lg:text-[70px] text-[40px] text-yellow-600 ${style.ctn}`}>2+</h1>
-                   <h4 className={`flex flex-col lg:text-[20px] text-[15px] ${style.yrs}`}>years of coding <span>experience</span> </h4> 
+                   <h1 className={`xl:text-[70px] lg:text-[50px] text-[40px] text-yellow-600 ${style.ctn}`}>2+</h1>
+                   <h4 className={`flex flex-col lg:text-[15px] xl:text-[20px] text-[15px] ${style.yrs}`}>years of coding <span>experience</span> </h4> 
                     </div> 
 
                     <div>
-                   <h1 className={`lg:text-[70px] text-[40px] text-yellow-600 ${style.ctn}`}>8+</h1>
-                   <h4 className={`flex flex-col lg:text-[20px] text-[15px] ${style.yrs}`}>completed <span>projects</span></h4> 
+                   <h1 className={`xl:text-[70px] lg:text-[50px] text-[40px] text-yellow-600 ${style.ctn}`}>8+</h1>
+                   <h4 className={`flex flex-col lg:text-[15px] xl:text-[20px] text-[15px] ${style.yrs}`}>completed <span>projects</span></h4> 
                     </div> 
                 </div>
 
